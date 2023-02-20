8296c769ceff907d624772e14d516547e91b2b81
Nick Romo <nickromo2121@gmail.com>
2023-02-20 20:21:37 +0100

    added codewars solution

---
+++ 8kyu/beginner-series-clock.js
@@ -0,0 +1,11 @@
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+
+
+//My solution
+
+function past(h, m, s){
+    return ((h*3600)+(m*60)+s)*1000;
+  }
