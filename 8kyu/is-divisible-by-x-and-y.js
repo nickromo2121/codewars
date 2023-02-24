f9639fdbc8d7ce41958ddbd82affef122cd5d6a8
Nick Romo <nickromo2121@gmail.com>
2023-02-25 00:00:48 +0100

    added codewars solution

---
+++ 8kyu/is-divisible-by-x-and-y.js
@@ -0,0 +1,6 @@
+//Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+
+//My solution
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0;
+  }
